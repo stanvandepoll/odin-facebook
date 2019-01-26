cfdc86cf5c12625746cd3e5cb94057cecbe2b304
Stan van de Poll <swvandepoll@gmail.com>
2019-01-26 14:49:01 +0100

    add domain option in config.action_mailer.smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
+ :domain               => "gmail.com"
  :user_name            => ENV['gmail_username'],
  :password             => ENV['gmail_password'],
  :authentication       => "plain",
